d7fa30b3d0f97ba7b9085cab4a069acf95deb70f
Danwoltrs <daniel@wolthers.com>
2026-06-29 10:50:48 -0300

    test: jsdom pointer/resize polyfills for combobox interaction tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,18 @@
 import '@testing-library/jest-dom/vitest'
+
+// jsdom lacks the pointer-capture / layout APIs that Radix Popover + cmdk call
+// when opening. Stub them so combobox (SearchableSelect) interactions work under test.
+if (typeof window !== 'undefined') {
+  if (!Element.prototype.hasPointerCapture) Element.prototype.hasPointerCapture = () => false
+  if (!Element.prototype.setPointerCapture) Element.prototype.setPointerCapture = () => {}
+  if (!Element.prototype.releasePointerCapture) Element.prototype.releasePointerCapture = () => {}
+  if (!Element.prototype.scrollIntoView) Element.prototype.scrollIntoView = () => {}
+  if (!('ResizeObserver' in window)) {
+    // eslint-disable-next-line @typescript-eslint/no-extraneous-class
+    ;(window as unknown as { ResizeObserver: unknown }).ResizeObserver = class {
+      observe() {}
+      unobserve() {}
+      disconnect() {}
+    }
+  }
+}
